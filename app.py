195fba5437db3e046e91116d589ccb9cdd758365
Aaron Hawkey <aaronhawkey48@gmail.com>
2018-08-26 12:46:50 -0500

    broken Oauth. Fixing.

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 from flask import Flask, request, redirect, url_for, render_template, jsonify, flash, make_response
 from flask import session as login_session
 from flask import jsonify
+import requests
 # Database Imports
 from database_setup import Base, User, Item, Category
 from sqlalchemy.ext.declarative import declarative_base
@@ -90,7 +91,7 @@ def login():
         state = ''.join(random.choice(string.ascii_uppercase + string.digits)
                     for x in xrange(32))
         login_session['state'] = state
-        return render_template('login.html')
+        return render_template('login.html', state = state)
 
     if request.method == 'POST':
         user = session.query(User).filter_by(
@@ -130,15 +131,88 @@ def logout():
 def gconnect():
 
     # Validate the State Token
-    if 'state' not in login_session:
+    if request.args.get('state') != login_session['state']:
         flash('State token incorrect. Please try again.')
         return redirect(url_for('index'))
 
     code = request.data
 
+    # Try to upgrade code
+    try:
+        oauth_flow = flow_from_clientsecrets('client_secrets.json', scope='')
+        oauth_flow.redirect_uri = 'postmessage'
+        credentials = oauth_flow.step2_exchange(code)
+    except FlowExchangeError:
+        response = make_response(
+            json.dumps('Failed to upgrade the authorization code.'), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
 
+    # Check access token
+    access_token = credentials.access_token
+    url = ('https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=%s'
+           % access_token)
+    h = httplib2.Http()
+    result = json.loads(h.request(url, 'GET')[1])
 
-    return 'Blah'
+    # Check for error in access
+    if result.get('error') is not None:
+        response = make_response(json.dumps(result.get('error')), 500)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    # Check user permission
+    gplus_id = credentials.id_token['sub']
+    if result['user_id'] != gplus_id:
+        response = make_response(
+            json.dumps("Token's user ID doesn't match given user ID."), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+    
+    # Checking validity for App
+    if result['issued_to'] != CLIENT_ID:
+        response = make_response(
+            json.dumps("Token's client ID does not match app's."), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    # Checking current session
+    stored_access_token = login_session.get('access_token')
+    stored_gplus_id = login_session.get('gplus_id')
+    if stored_access_token is not None and gplus_id == stored_gplus_id:
+        response = make_response(json.dumps('Current user is already connected.'),
+                                 200)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    # Storing token in session
+    login_session['access_token'] = credentials.access_token
+    login_session['gplus_id'] = gplus_id
+
+    # Fetching user information
+    userinfo_url = "https://www.googleapis.com/oauth2/v1/userinfo"
+    params = {'access_token': credentials.access_token, 'alt': 'json'}
+    answer = requests.get(userinfo_url, params=params)
+
+    user_data = answer.json()
+
+    user = session.query(User).filter_by(email = user_data['email']).first()
+
+    if user is not None:
+        login_session['user_id'] = user.id
+        flash('Logged in as %s' % user.username)
+        return redirect(url_for('index'))
+    
+
+    newUser = User(username = user_data['email'], email=user_data['email'])
+    session.add(newUser)
+    session.commit()
+    userQuery = session.query(User).filter_by(email = newUser.email).first()
+    login_session['user_id']= userQuery.id
+
+    return redirect(url_for('index'))
+    
+
 
 
 
